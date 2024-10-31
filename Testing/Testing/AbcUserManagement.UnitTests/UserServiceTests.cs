cc27b26b00e3e093006aa4ced0aeb07457e3ba51
reynananislag <anislar@ph.ibm.com>
2024-11-01 06:53:42 +0800

    revert negative tests

--- Testing/Testing/AbcUserManagement.UnitTests/UserServiceTests.cs
+++ Testing/Testing/AbcUserManagement.UnitTests/UserServiceTests.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using AbcUserManagement.DataAccess;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace AbcUserManagement.UnitTests
 {
@@ -40,6 +41,19 @@ namespace AbcUserManagement.UnitTests
         }
 
         [Test]
+        public void AddUserAsync_ShouldThrowUnauthorizedAccessException_WhenCompanyIdMismatch()
+        {
+            // Arrange
+            var user = new User { Username = "test", PasswordHash = "hash", Role = Role.User, CompanyId = 2 };
+            var companyId = 1;
+            var role = Role.Admin.ToString();
+            var createdBy = "admin";
+
+            // Act & Assert
+            Assert.ThrowsAsync<UnauthorizedAccessException>(() => _userService.AddUserAsync(user, companyId, role, createdBy));
+        }
+
+        [Test]
         public async Task UpdateUserAsync_ShouldCallRepositoryMethod_ForAdmin()
         {
             // Arrange
@@ -56,6 +70,19 @@ namespace AbcUserManagement.UnitTests
         }
 
         [Test]
+        public void UpdateUserAsync_ShouldThrowUnauthorizedAccessException_WhenCompanyIdMismatch()
+        {
+            // Arrange
+            var user = new User { Id = 1, Username = "test", PasswordHash = "hash", Role = Role.User, CompanyId = 2 };
+            var companyId = 1;
+            var role = Role.Admin.ToString();
+            var modifiedBy = "admin";
+
+            // Act & Assert
+            Assert.ThrowsAsync<UnauthorizedAccessException>(() => _userService.UpdateUserAsync(user, companyId, role, modifiedBy));
+        }
+
+        [Test]
         public async Task DeleteUserAsync_ShouldCallRepositoryMethod_ForAdmin()
         {
             // Arrange
@@ -68,6 +95,20 @@ namespace AbcUserManagement.UnitTests
 
             // Assert
             _userRepositoryMock.Verify(repo => repo.DeleteUserAsync(userId), Times.Once);
+        }
+
+        [Test]
+        public void DeleteUserAsync_ShouldThrowUnauthorizedAccessException_WhenCompanyIdMismatch()
+        {
+            // Arrange
+            var userId = 1;
+            var companyId = 1;
+            var role = Role.Admin.ToString();
+            var user = new User { Id = userId, CompanyId = 2 };
+            _userRepositoryMock.Setup(repo => repo.GetUserByIdAsync(userId)).ReturnsAsync(user);
+
+            // Act & Assert
+            Assert.ThrowsAsync<UnauthorizedAccessException>(() => _userService.DeleteUserAsync(userId, companyId, role));
         }
 
         [Test]
@@ -89,6 +130,72 @@ namespace AbcUserManagement.UnitTests
             // Assert
             Assert.AreEqual(1, result.Count());
             Assert.AreEqual("user1", result.First().Username);
+        }
+
+        [Test]
+        public async Task GetUsersByCompanyIdAsync_ShouldReturnEmptyList_WhenNoUsersExist()
+        {
+            // Arrange
+            var companyId = 1;
+            var role = Role.User.ToString();
+            var users = new List<User>();
+            _userRepositoryMock.Setup(repo => repo.GetUsersByCompanyIdAsync(companyId)).ReturnsAsync(users);
+
+            // Act
+            var result = await _userService.GetUsersByCompanyIdAsync(companyId, role).ConfigureAwait(false);
+
+            // Assert
+            Assert.IsEmpty(result);
+        }
+
+        [Test]
+        public async Task GetUserByIdAsync_ShouldReturnNull_WhenUserDoesNotExist()
+        {
+            // Arrange
+            var userId = 1;
+            var companyId = 1;
+            var role = Role.Admin.ToString();
+            _userRepositoryMock.Setup(repo => repo.GetUserByIdAsync(userId)).ReturnsAsync((User)null);
+
+            // Act
+            var result = await _userService.GetUserByIdAsync(userId, companyId, role).ConfigureAwait(false);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public async Task GetUserByIdAsync_ShouldReturnNull_WhenCompanyIdMismatch()
+        {
+            // Arrange
+            var userId = 1;
+            var companyId = 1;
+            var role = Role.Admin.ToString();
+            var user = new User { Id = userId, CompanyId = 2 };
+            _userRepositoryMock.Setup(repo => repo.GetUserByIdAsync(userId)).ReturnsAsync(user);
+
+            // Act
+            var result = await _userService.GetUserByIdAsync(userId, companyId, role).ConfigureAwait(false);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public async Task GetUserByIdAsync_ShouldReturnNull_WhenFlatUserTriesToAccessAdmin()
+        {
+            // Arrange
+            var userId = 1;
+            var companyId = 1;
+            var role = Role.User.ToString();
+            var user = new User { Id = userId, CompanyId = companyId, Role = Role.Admin };
+            _userRepositoryMock.Setup(repo => repo.GetUserByIdAsync(userId)).ReturnsAsync(user);
+
+            // Act
+            var result = await _userService.GetUserByIdAsync(userId, companyId, role).ConfigureAwait(false);
+
+            // Assert
+            Assert.IsNull(result);
         }
     }
 }
